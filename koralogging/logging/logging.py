789bd71802290770b9a2a84f2769ec7024ce8e94
Aquele Astro Dev <astrochanneloficial@gmail.com>
2025-12-21 15:20:22 -0300

    Primeira Versão!

--- koralogging/logging/logging.py
+++ koralogging/logging/logging.py
@@ -1,1 +1,7 @@
-#
+from koralogging import StartLogging, logging
+
+logging.starter (
+    start = StartLogging,
+    loggingClient = logging,
+    on_ready = lambda: logging.info("Kora iniciado!")
+)
