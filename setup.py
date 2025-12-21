78e39fe5ad91bbe95fcecc5ca0bec2cbae750e49
Aquele Astro Dev <astrochanneloficial@gmail.com>
2025-12-21 15:24:32 -0300

    Create setup.py for Kora SDK
    Add setup.py for Kora SDK with metadata

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="kora",
+    version="1.0.0",
+    description="Uma SDK Python para fazer o logging(registro) de Clients",
+    author="umastrodev12",
+    packages=find_packages(),
+    python_requires=">3.8",
+    url="https://github.com/PerfectTeaBR/Kora"
+
+)
